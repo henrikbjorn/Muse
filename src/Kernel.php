ed7334d0e6193a7c3462354b0a7e3ceaa52431c6
Henrik Bjornskov <hb@peytz.dk>
2015-11-05 15:51:58 +0100

    configuration

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,8 +5,10 @@ namespace Muse;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Routing\RouteCollectionBuilder;
+use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Bundle\FrameworkBundle;
+use Symfony\Bundle\MonologBundle;
 
 class Kernel extends \Symfony\Component\HttpKernel\Kernel
 {
@@ -16,27 +18,37 @@ class Kernel extends \Symfony\Component\HttpKernel\Kernel
     {
         return [
             new FrameworkBundle\FrameworkBundle(),
+            new MonologBundle\MonologBundle(),
         ];
+    }
+
+    public function getRootDir()
+    {
+        return __DIR__ . '/..';
     }
 
     public function getCacheDir()
     {
-        return $this->rootDir . '/../var/cache/' . $this->environment;
+        return $this->rootDir . '/var/cache/' . $this->environment;
     }
 
     public function getLogDir()
     {
-        return $this->rootDir . '/../var/logs/' . $this->environment;
+        return $this->rootDir . '/var/logs/' . $this->environment;
     }
 
-    protected function configureRoutes(RouteCollectionBuilder $routes)
+    public function defaultAction()
     {
+        return new Response('DefaultAction');
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder &$routes)
+    {
+        $routes = $routes->import($this->rootDir . '/src/Resources/routing/routing.xml', null);
     }
 
     protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
     {
-        $c->loadFromExtension('framework', [
-            'secret' => 'justanonsecuresecret',
-        ]);
+        $loader->load($this->rootDir . '/config/' . $this->environment . '.yml');
     }
 }
