8ce5469890c519e56cd5c3043046ba72120ac0f0
Henrik Bjornskov <hb@peytz.dk>
2015-12-07 11:38:27 +0100

    Fix requirements for sf 3.0 components

--- src/functions.php
+++ src/functions.php
@@ -2,6 +2,11 @@
 
 namespace Muse;
 
+
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\TerminableInterface;
+use Symfony\Component\HttpFoundation\Request;
+
 function is_debug($env)
 {
     return in_array($env, ['test', 'dev']);
@@ -13,4 +18,16 @@ function create_application()
     $app->loadClassCache();
 
     return $app;
+}
+
+
+function run(HttpKernelInterface $app, Request $request = null)
+{
+    $request = $request ?: Request::createFromGlobals();
+
+    $response = $app->handle($request);
+    $response->send();
+    if ($app instanceof TerminableInterface) {
+        $app->terminate($request, $response);
+    }
 }
--- web/index.php
+++ web/index.php
@@ -2,8 +2,6 @@
 
 namespace Muse;
 
-use function Stack\run;
-
 require __DIR__ . '/../vendor/autoload.php';
 
 run(create_application());
